866ff2ca6e1bf22dedbfa2df2ecff34b988d8aae
xpertfs <irayhan@xpertfieldservices.com>
2025-05-16 23:55:34 -0400

    Remove build directory from git tracking and update .gitignore

--- build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '7d8aa53374b551114ccb');
--- build/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => 'cc64c2d054aa1595d68a');
